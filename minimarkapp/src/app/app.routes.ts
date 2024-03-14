b4a3ad8a5c1ec03fdffe4495fde1c475a6a5122e
dafesigo04 <dfernandosilva@ucompensar.edu.co>
2024-03-13 19:07:19 -0500

    new plantilla

--- minimarkapp/src/app/app.routes.ts
+++ minimarkapp/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PlantillaComponent } from './plantilla/plantilla.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'plantilla', component: PlantillaComponent },
+    {path: '',  redirectTo: '/plantilla', pathMatch: 'full' },
+];
